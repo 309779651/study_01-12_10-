e4f3855fe8e00719dcaea4d1bb5eb29e4ca85206
309779651 <309779651@qq.com>
2018-12-12 10:44:16 +0800

    Main方法成功运行

---
+++ tomcat_Embeded_test2/src/main/java/tomcat_Embeded_test2/Main.java
@@ -0,0 +1,37 @@
+package tomcat_Embeded_test2;
+
+import java.io.File;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+public class Main {
+
+	public static void main(String[] args) throws LifecycleException,
+	ServletException {
+
+
+	String webappDirLocation = "src/main/webapp/";
+	Tomcat tomcat = new Tomcat();
+	tomcat.setPort(9090);
+	StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(
+	webappDirLocation).getAbsolutePath());
+	ctx.setReloadable(false);
+	File additionWebInfClasses = new File("target/classes");
+	WebResourceRoot resources = new StandardRoot(ctx);
+	resources.addPreResources(new DirResourceSet(resources,
+	"/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(),
+	"/"));
+	tomcat.start();
+	tomcat.getServer().await();
+
+
+	}
+	
+}
